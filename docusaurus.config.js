f90148953edf3f9b2d8b7e1253692ef4b464a791
bestevan01 <44205515+bestevan01@users.noreply.github.com>
2025-04-30 01:23:15 +0900

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,14 @@
+themeConfig:{
+  navbar:{
+    title:'SpreadsheetMacro',
+      logo:{
+      alt: 'SpreadsheetMacro Logo',
+        src: './logo.png',
+        },
+        items: [
+      { to: '/', label: '홈', position: 'left' },
+      { to: '/privacy-policy', label: '개인정보 처리방침', position: 'right' },
+      { to: '/terms-of-service', label: '서비스 약관', position: 'right' }
+    ]
+  },
+}
